9d083468dcd908d013fef937d87f372a8a1626b0
IN2DM3B_10 <IN2DM3B_10@H015112.NET>
2020-01-14 13:17:02 +0100

    Clase ModeloDatos para almacenar de la BD

--- app/src/main/java/com/example/retofinalandroid/Inicio.java
+++ app/src/main/java/com/example/retofinalandroid/Inicio.java
@@ -2,8 +2,11 @@ package com.example.retofinalandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ImageView;
+
+
 
 public class Inicio extends AppCompatActivity {
 
@@ -11,6 +14,31 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio);
+        boolean a;
 
+    }
+
+    private class descargarJSOn extends AsyncTask<Void, Void, Boolean> {
+        private String url = "188.213.5.150";
+        private String user = "ldmj";
+        private String pass = "ladamijo";
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try {
+
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean b) {
+
+        }
     }
 }
